32d4ebd1db5ffc6d2adbbd20be38baacf7324a77
moeka <moeka@moeka-PC>
2017-07-23 11:30:42 +0900

    Replace a transition file and correct the mistakes in the code

--- src/TraceGenerator.java
+++ src/TraceGenerator.java
@@ -19,7 +19,7 @@ import java.util.Random;
 public class TraceGenerator {
 
 	// Transition description of environment model (envModel_small/large.txt)
-	private static String transitionsName = "envModel_large.txt";
+	private static String transitionsName = "envModel_small.txt";
 
 	// Name of output file
 	private static String traceName = "Trace.txt";
@@ -69,8 +69,10 @@ public class TraceGenerator {
 					s.num = states.size();
 					states.add(s);
 
-					if (line[line.length - 1].endsWith(",") || line[line.length - 1].endsWith(".")) {
-						m1 = Integer.parseInt(line[3].substring(1, line[3].length() - 2));
+					if (line[line.length - 1].endsWith(",")
+							|| line[line.length - 1].endsWith(".")) {
+						m1 = Integer.parseInt(line[3].substring(1,
+								line[3].length() - 2));
 						m2 = line[1].substring(2 - 1);
 						states.get(states.size() - 1).map.put(m1, m2);
 						states.get(states.size() - 1).keys.add(m1);
@@ -80,8 +82,10 @@ public class TraceGenerator {
 						states.get(states.size() - 1).map.put(m1, m2);
 						states.get(states.size() - 1).keys.add(m1);
 					}
-				} else if (line[line.length - 1].endsWith(",") || line[line.length - 1].endsWith(".")) {
-					m1 = Integer.parseInt(line[4].substring(1, line[4].length() - 2));
+				} else if (line[line.length - 1].endsWith(",")
+						|| line[line.length - 1].endsWith(".")) {
+					m1 = Integer.parseInt(line[4].substring(1,
+							line[4].length() - 2));
 					m2 = line[2].substring(2 - 1);
 					states.get(states.size() - 1).map.put(m1, m2);
 					states.get(states.size() - 1).keys.add(m1);
@@ -100,13 +104,12 @@ public class TraceGenerator {
 		}
 	}
 
-
 	/**
 	 * Generate trace file
 	 *
 	 * @param random
-	 *            When two post-condition are observed, true.
-	 *            When only one post-condition is observed, false.
+	 *            When two post-condition are observed, true. When only one
+	 *            post-condition is observed, false.
 	 *
 	 */
 	public static void Generate(boolean random) {
@@ -120,7 +123,8 @@ public class TraceGenerator {
 
 		try {
 			File file = new File(traceName);
-			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(
+					file)));
 
 			stateNum = states.get(0).keys.get(0);
 
@@ -133,13 +137,57 @@ public class TraceGenerator {
 						|| s.map.get(s.keys.get(r)).equals("wait")) {
 					count--;
 					stateNum = s.keys.get(r);
-				} else if (random == false) {
-					// When only one post-condition is observed
-					pw.println(s.map.get(s.keys.get(r)));
-					stateNum = s.keys.get(r);
-				} else if (transitionsName.equals("envModel_small.txt")){
-					// When two post-conditions are observed in small case
-					if(prepre.startsWith("arrive")){
+				} else if (transitionsName.startsWith("envModel_large")) {
+					// GenerateLarge(random, s, r);
+					if (random == false) {
+						// When only one post-condition is observed
+						pw.println(s.map.get(s.keys.get(r)));
+						stateNum = s.keys.get(r);
+					} else {
+						// When two post-conditions are observed in large case
+						if (pre.equals("move.s") || pre.equals("move.n")
+								|| pre.equals("move.e") || pre.equals("move.w")) {
+							if (prepre.startsWith("arrive[")) {
+								rr = rnd.nextInt(2);
+								if (rr == 0) {
+									pw.println(s.map.get(s.keys.get(r)));
+									stateNum = s.keys.get(r);
+									prepre = pre;
+									pre = s.map.get(s.keys.get(r));
+								} else {
+									pw.println(prepre);
+									count++;
+									pw.println(pre);
+								}
+							} else {
+								pw.println(s.map.get(s.keys.get(r)));
+								stateNum = s.keys.get(r);
+								prepre = pre;
+								pre = s.map.get(s.keys.get(r));
+							}
+						} else {
+							pw.println(s.map.get(s.keys.get(r)));
+							stateNum = s.keys.get(r);
+							prepre = pre;
+							pre = s.map.get(s.keys.get(r));
+						}
+					}
+				} else if (transitionsName.startsWith("envModel_small")) {
+					if (random == false) {
+						// When only one post-condition is observed
+						if (pre.equals("putdown")) {
+							pw.println(s.map.get(s.keys.get(0)));
+							stateNum = s.keys.get(0);
+						} else if (pre.equals("pickup")) {
+							pw.println(s.map.get(s.keys.get(1)));
+							stateNum = s.keys.get(1);
+						} else {
+							pw.println(s.map.get(s.keys.get(r)));
+							stateNum = s.keys.get(r);
+						}
+						pre = s.map.get(stateNum);
+					} else if (prepre.startsWith("arrive")) {
+						// When two post-conditions are observed in small case
 						rr = rnd.nextInt(2);
 						if (rr == 0) {
 							pw.println(s.map.get(s.keys.get(r)));
@@ -150,34 +198,6 @@ public class TraceGenerator {
 							pw.println(prepre);
 							count++;
 							pw.println(pre);
-						}
-					}else{
-						pw.println(s.map.get(s.keys.get(r)));
-						stateNum = s.keys.get(r);
-						prepre = pre;
-						pre = s.map.get(s.keys.get(r));
-					}
-				} else {
-					// When two post-conditions are observed in large case
-					if (pre.equals("move.s") || pre.equals("move.n")
-							|| pre.equals("move.e") || pre.equals("move.w")) {
-						if (prepre.startsWith("arrive[")) {
-							rr = rnd.nextInt(2);
-							if (rr == 0) {
-								pw.println(s.map.get(s.keys.get(r)));
-								stateNum = s.keys.get(r);
-								prepre = pre;
-								pre = s.map.get(s.keys.get(r));
-							} else {
-								pw.println(prepre);
-								count++;
-								pw.println(pre);
-							}
-						} else {
-							pw.println(s.map.get(s.keys.get(r)));
-							stateNum = s.keys.get(r);
-							prepre = pre;
-							pre = s.map.get(s.keys.get(r));
 						}
 					} else {
 						pw.println(s.map.get(s.keys.get(r)));
@@ -193,5 +213,4 @@ public class TraceGenerator {
 			System.out.println(e);
 		}
 	}
-
 }
